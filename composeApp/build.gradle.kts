4f65ae12c205e52e101a38bb9319d67066e1d5e4
Fabian Schmitthenner <development@schmitthenner.eu>
2024-07-05 22:15:20 +0200

    Prepare new release and change name of Android app

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -82,8 +82,8 @@ android {
         applicationId = "io.github.potsdam_pnp.initiative_tracker"
         minSdk = libs.versions.android.minSdk.get().toInt()
         targetSdk = libs.versions.android.targetSdk.get().toInt()
-        versionCode = 1
-        versionName = "1.0"
+        versionCode = 2
+        versionName = "1.1"
     }
     packaging {
         resources {
