27668d02ca720695084ec6fbedb0b990cb22185e
Tillmann Rendel <tillmann.rendel@posteo.de>
2024-07-07 23:53:49 +0200

    Avoid FHS in nix.shell
    Problem: namespace leak because gradle daemon was started in the FHS env
    Solution: replace weird FHS by weird LD tricks
    
    This setup allows me to run both `gradle run` (to start the desktop app)
    and `gradle wasmJsBrowserDevelopmentRun` (to start the web app)
    inside `nix develop` on NixOS.
    
    For some reason, the website is not opened automatically in the browser,
    but the localhost URL is printed and can be opened manually.

--- shell.nix
+++ shell.nix
@@ -1,12 +1,40 @@
 { pkgs ? import <nixpkgs> {} }:
 
 let
-  gradle = pkgs.buildFHSEnv {
+  # used in ./gradlew script
+  bin-path = pkgs.lib.makeBinPath [
+    pkgs.coreutils
+    pkgs.findutils
+    pkgs.gnused
+  ];
+
+  # needed for desktop target (via downloaded skiko)
+  ld-library-path = pkgs.lib.makeLibraryPath [
+    pkgs.libGL
+  ];
+
+  # needed for wasm target (via downloaded node)
+  # (requires https://github.com/Mic92/nix-ld)
+  nix-ld = "${pkgs.stdenv.cc}/nix-support/dynamic-linker";
+  nix-ld-library-path = pkgs.lib.makeLibraryPath [
+    pkgs.stdenv.cc.cc.lib
+  ];
+
+  gradle = pkgs.writeShellApplication {
     name = "gradle";
-    targetPkgs = pkgs: [
-      pkgs.jdk
-    ];
-    runScript = "./gradlew";
+    text = ''
+      if [[ ! -f "./gradlew" ]]; then
+        echo "Cannot find gradle wrapper (./gradlew)" >&1
+        exit 1
+      fi
+
+      PATH=${bin-path} \
+      LD_LIBRARY_PATH=${ld-library-path} \
+      NIX_LD_LIBRARY_PATH=${nix-ld-library-path} \
+      NIX_LD=$(cat "${nix-ld}") \
+      JAVA_HOME=${pkgs.jre} \
+      exec "./gradlew" "$@"
+    '';
   };
 in
 
