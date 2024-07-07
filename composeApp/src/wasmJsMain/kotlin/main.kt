dc4162327b7d5108319c7351fc69d038dc2f8555
Fabian Schmitthenner <development@schmitthenner.eu>
2024-07-07 13:59:42 +0200

    Support #comma-seperated-characters links also in browser-based version, as we started doing in b5645f4 for the Android app

--- composeApp/src/wasmJsMain/kotlin/main.kt
+++ composeApp/src/wasmJsMain/kotlin/main.kt
@@ -1,10 +1,19 @@
 import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.window.ComposeViewport
 import kotlinx.browser.document
+import kotlinx.browser.window
+
+fun appData(): String? {
+    val hash = window.location.hash
+    if (hash == "")
+        return null
+    val afterHash = hash.substring(1)
+    return afterHash // TODO call something like decodeUriCompoenent
+}
 
 @OptIn(ExperimentalComposeUiApi::class)
 fun main() {
     ComposeViewport(document.body!!) {
-        App()
+        App(appData())
     }
 }
