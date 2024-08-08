62fafd94ac18b0dafdf7d8be665396e0d4e5d57b
Fabian Schmitthenner <development@schmitthenner.eu>
2024-08-08 21:53:45 +0200

    Add a few more simple tests

--- composeApp/src/commonTest/kotlin/ActionStateTests.kt
+++ composeApp/src/commonTest/kotlin/ActionStateTests.kt
@@ -10,4 +10,31 @@ class ActionStateTests {
         assertEquals(emptyList(), predicted)
         assertNull(state.currentTurn())
     }
+
+    @Test
+    fun twoCharacters() {
+        val state = State2(
+            listOf(
+                ChangeInitiative("character1", 5),
+                ChangeInitiative("character2", 10)
+            )
+        )
+        val predicted = state.predictNextTurns(withCurrent = false)
+        assertEquals(listOf("character2", "character1"), predicted.map { it.key })
+        assertNull(state.currentTurn())
+    }
+
+    @Test
+    fun twoCharacters2() {
+        val state = State2(
+            listOf(
+                ChangeInitiative("character1", 5),
+                ChangeInitiative("character2", 10),
+                StartTurn("character1")
+            )
+        )
+        val predicted = state.predictNextTurns(withCurrent = false)
+        assertEquals(listOf("character2", "character1"), predicted.map { it.key })
+        assertEquals("character1", state.currentTurn())
+    }
 }
