44deb4250e3c59ee62f681a1cd5040ca221bd1bf
Fabian Schmitthenner <development@schmitthenner.eu>
2025-05-30 11:16:56 +0200

    prepare release

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -105,8 +105,8 @@ android {
     applicationId = "io.github.potsdam_pnp.initiative_tracker"
     minSdk = libs.versions.android.minSdk.get().toInt()
     targetSdk = libs.versions.android.targetSdk.get().toInt()
-    versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 124
-    versionName = System.getenv("VERSION_NAME") ?: "0.2.0"
+    versionCode = System.getenv("VERSION_CODE")?.toIntOrNull() ?: 125
+    versionName = System.getenv("VERSION_NAME") ?: "0.2.1"
   }
   packaging {
     resources { excludes += "/META-INF/{AL2.0,LGPL2.1,INDEX.LIST,io.netty.versions.properties}" }
